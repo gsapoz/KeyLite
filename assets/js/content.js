3603a69f15a68e35354f7de37b28924234b3dff3
Gary <gtech235@gmail.com>
2023-04-24 14:04:16 -0400

    Imported pdf.js and made it so we can read text and open new web pagepage on submit

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,16 +1,121 @@
+// Declare Form Submit Buttons
 let btnOpen = document.getElementById("openPDF");
-btnOpen.addEventListener("click", () => {
-  let input = document.createElement("input");
-  input.type = "file";
-  input.accept = "application/pdf";
-  input.click();
+let btnUpdate = document.getElementById("updatePDF");
 
-  input.addEventListener("change", () => {
-    let file = input.files[0];
-    let url = URL.createObjectURL(file);
-    window.open(url, "_blank");
-  });
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  // Get the URL of the current tab
+  var currentUrl = tabs[0].url;
+
+  // Get the pathname of the current tab
+  var currentPathname = new URL(currentUrl).pathname;
+
+  if (currentPathname.toLocaleLowerCase().endsWith(".pdf")) {
+    btnOpen.classList.add("inactive");
+    btnUpdate.addEventListener("click", () => {
+      // Call the extractPdfText function with the PDF file URL
+      extractPdfText(currentUrl)
+        .then((text) => {
+          createPageFromText(text);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
+  } else {
+    btnUpdate.classList.add("inactive");
+    btnOpen.addEventListener("click", () => {
+      let input = document.createElement("input");
+      input.type = "file";
+      input.accept = "application/pdf";
+      input.click();
+
+      input.addEventListener("change", () => {
+        let file = input.files[0];
+        let url = URL.createObjectURL(file);
+
+        // Get the original file name and append it to the URL
+        let fileName = file.name;
+        let newUrl = url + "#" + fileName;
+
+        // Call the extractPdfText function with the PDF file URL
+        extractPdfText(newUrl)
+          .then((text) => {
+            createPageFromText(text);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      });
+    });
+  }
 });
+
+// Create a function to extract text from a PDF file
+async function extractPdfText(pdfUrl) {
+  // Load the PDF document
+  const loadingTask = pdfjsLib.getDocument(pdfUrl);
+  const pdfDocument = await loadingTask.promise;
+
+  // Get the text content of each page
+  const textContent = [];
+  for (let i = 1; i <= pdfDocument.numPages; i++) {
+    const page = await pdfDocument.getPage(i);
+    const content = await page.getTextContent();
+    textContent.push(content);
+  }
+
+  // Concatenate the text content of all pages
+  const text = textContent
+    .map((content) => content.items.map((item) => item.str).join(""))
+    .join("");
+
+  // Return the extracted text
+  return text;
+}
+
+function createPageFromText(text) {
+  // Create a new HTML document
+  const html = `
+    <html>
+      <head>
+        <title>Extracted Text</title>
+      </head>
+      <body>
+        <p>${text}</p>
+        <style>
+          html {
+            background: darkgrey;    
+          }
+          body {
+            background: ghostwhite;
+            height: 200vh; 
+            width: 84%;
+            margin: auto;
+            margin-top: 65px;
+            padding: 50px;
+            border: 1px solid black;
+            border-radius: 5px;
+          }
+          p { 
+            font-size:25px;
+          }
+        </style>
+      </body>
+    </html>
+  `;
+
+  // Create a new blob from the HTML string
+  const blob = new Blob([html], { type: "text/html" });
+
+  // Create a URL for the blob
+  const url = URL.createObjectURL(blob);
+
+  // Open the URL in a new tab
+  window.open(url, "_blank");
+
+  // Clean up the URL object
+  URL.revokeObjectURL(url);
+}
 
 // Define a function to bolden the first three letters of each word in a string
 function boldenFirstThreeLetters(str) {
